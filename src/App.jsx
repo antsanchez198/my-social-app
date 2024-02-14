0534b082b3e6becfeda8c53219f93c2215f61d70
Anthony Sanchez <antsanchez198@gmail.com>
2024-02-14 00:09:39 -0500

    Added Navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,22 @@
-import { useState } from 'react'
-import './App.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Header from './components/Header'
+import HomeFeed from './pages/HomeFeed'
+import Login from './pages/Login'
+import SignUp from './pages/SignUp'
+import Post from './pages/Post'
 
 function App() {
 
   return (
-    <>
-     <Header/>
-    </>
+    <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<HomeFeed />}></Route>
+          <Route path="/login" element={<Login />}></Route>
+          <Route path="/sign-up" element={<SignUp />}></Route>
+          <Route path="/post" element={<Post />}></Route>
+        </Routes>
+      </BrowserRouter>
   )
 }
 
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,9 @@
-
+import { useNavigate } from "react-router-dom"
 
 const Header = () => {
+
+    const navigate = useNavigate();
+
     return (
         <div className="bg-slate-700 w-full h-15">
             <div className="max-w-[90%] mx-auto flex justify-between items-center py-3">
@@ -8,9 +11,9 @@ const Header = () => {
                     <img></img>
                 </div>
                 <ul className="flex space-x-5 text-white">
-                    <li className="cursor-pointer">Feed</li>
-                    <li className="cursor-pointer">Post</li>
-                    <li className="cursor-pointer">Account</li>
+                    <li className="cursor-pointer" onClick={() => navigate("/")}>Feed</li>
+                    <li className="cursor-pointer" onClick={() => navigate("/post")}>Post</li>
+                    <li className="cursor-pointer" onClick={() => navigate("/")}>Account</li>
                 </ul>
             </div>
         </div>
