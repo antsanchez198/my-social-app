c5770254273d908c96b9863ff1dccc58c6c88687
Anthony Sanchez <antsanchez198@gmail.com>
2024-09-01 01:27:50 -0400

    images not showing, needs fix

--- src/components/DoubleImgTemp.jsx
+++ src/components/DoubleImgTemp.jsx
@@ -1,5 +1,20 @@
-export default function DoubleImgTemp () {
+import { useEffect } from "react"
+
+export default function DoubleImgTemp({ images }) {
+
+    useEffect(() => {
+        console.log(images)
+    })
+
     return (
-        <>DoubleImgTemp</>
+        <div>
+            {images && images.map((image) => (                
+                <img
+                    src={image.image[0]}
+                    // alt={post.caption}
+                    className='object-cover w-full'
+                />
+            ))}
+        </div>
     )
 }
